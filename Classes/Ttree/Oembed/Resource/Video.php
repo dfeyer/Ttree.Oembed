8107a4dfb088b4e70f6e0197ec48d8e030e6ab82
Karsten Dambekalns <karsten@typo3.org>
2013-08-13 15:10:52 +0200

    [TASK] Adjust file docblocks to intended license
    As per the README the code is GPL, but the class level docblocks
    claimed LGPL.

--- Classes/Ttree/Oembed/Resource/Video.php
+++ Classes/Ttree/Oembed/Resource/Video.php
@@ -5,18 +5,8 @@ namespace Ttree\Oembed\Resource;
  * This script belongs to the Flow package "Ttree.Oembed".                *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License as published by the *
- * Free Software Foundation, either version 3 of the License, or (at your *
- * option) any later version.                                             *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
- * General Public License for more details.                               *
- *                                                                        *
- * You should have received a copy of the GNU Lesser General Public       *
- * License along with the script.                                         *
- * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
@@ -26,7 +16,6 @@ use TYPO3\Flow\Annotations as Flow;
 /**
  * oEmbed Video
  *
- * @package Ttree.Oembed
  * @author  Romain Ruetschi <romain.ruetschi@gmail.com>
  * @author  Dominique Feyer <dfeyer@ttree.ch>
  */
