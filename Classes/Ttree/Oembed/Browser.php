7caf3f56cf73a4d2cc47a5f6f4d741e398d33751
Dominique Feyer <dfeyer@ttree.ch>
2014-06-19 13:35:32 +0200

    [TASK] Use a simplified browser to fetch page content

--- Classes/Ttree/Oembed/Browser.php
+++ Classes/Ttree/Oembed/Browser.php
@@ -22,41 +22,39 @@ use TYPO3\Flow\Annotations as Flow;
 class Browser {
 
 	/**
-	 * @Flow\Inject
-	 * @var \TYPO3\Flow\Http\Client\Browser
-	 */
-	protected $browser;
-
-	/**
-	 * @Flow\Inject
-	 * @var \TYPO3\Flow\Http\Client\CurlEngine
-	 */
-	protected $browserRequestEngine;
-
-	/**
-	 * Initialize object
-	 */
-	public function initializeObject() {
-		$this->browser->setRequestEngine($this->browserRequestEngine);
-	}
-
-	/**
 	 * @param string $url
 	 * @return string
 	 * @throws Exception
 	 */
 	public function getContent($url) {
-		$response = $this->browser->request($url);
+		$options = array(
+			CURLOPT_CUSTOMREQUEST => "GET",
+			CURLOPT_POST => false,
+			CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_HEADER => false,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_AUTOREFERER => true,
+			CURLOPT_CONNECTTIMEOUT => 30,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_MAXREDIRS => 10,
+		);
 
-		if ($response->getStatusCode() !== 200) {
-			throw new Exception(
-				sprintf('HTTP request do not return 200 status code (%s)', $response->getStatusCode()),
-				1355878139
-			);
+		$handler = curl_init($url);
+		curl_setopt_array($handler, $options);
+		$content = curl_exec($handler);
+		$errorNumber = curl_errno($handler);
+		if ($errorNumber > 0) {
+			$errorMessage  = curl_error($handler);
+			throw new Exception(sprintf('Unable to get content, CURL error: %s, ', $errorNumber, $errorMessage), 1402851391);
 		}
+		$header  = curl_getinfo($handler);
+		if ($header['http_code'] !== 200) {
+			throw new Exception(sprintf('Unable to get content, URL %s response code: %s, ', $url, $header['http_code']), 1402851391);
+		}
+		curl_close($handler);
 
-		return $response->getContent();
+		return $content;
 	}
 }
-
-?>
