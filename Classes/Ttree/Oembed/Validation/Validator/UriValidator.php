c6367b4f46fc673a171edc298df406aa60308a6f
Dominique Feyer <dfeyer@ttree.ch>
2014-06-19 13:38:04 +0200

    [TASK] Add oEmbed URI validator

---
+++ Classes/Ttree/Oembed/Validation/Validator/UriValidator.php
@@ -0,0 +1,38 @@
+<?php
+namespace Ttree\Oembed\Validation\Validator;
+
+/*                                                                        *
+ * This script belongs to the Flow package "Ttree.Oembed".                *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use Ttree\Oembed\Consumer;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Validation\Validator\AbstractValidator;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class UriValidator extends AbstractValidator {
+
+	/**
+	 * Checks if the given value is a specific boolean value.
+	 *
+	 * @param mixed $value The value that should be validated
+	 * @return void
+	 * @api
+	 */
+	protected function isValid($value) {
+		$consumer = new Consumer();
+		$embedObject = $consumer->consume($value);
+		if ($embedObject === NULL) {
+			$this->addError('This URI has no oEmbed support.', 1403004582, array($value));
+		}
+	}
+
+}
