1ddafe1e926f909c0317bcc71442708c55e57d71
Karsten Dambekalns <karsten@dambekalns.de>
2017-01-18 16:20:59 +0100

    TASK: Update file headers blocks

--- Classes/ResourceFactory.php
+++ Classes/ResourceFactory.php
@@ -1,15 +1,15 @@
 <?php
 namespace Ttree\Oembed;
 
-/*                                                                        *
- * This script belongs to the Flow package "Ttree.Oembed".                *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU General Public License, either version 3 of the   *
- * License, or (at your option) any later version.                        *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
+/*
+ * This file is part of the Ttree.Oembed package.
+ *
+ * (c) Dominique Feyer <dfeyer@ttree.ch>
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
 
 use Ttree\Oembed\Resource\AbstractResource;
 use Ttree\Oembed\Resource\PostProcessor\PostProcessorInterface;
